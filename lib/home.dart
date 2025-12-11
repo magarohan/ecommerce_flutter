7b8ab4375a046d417cf3dd77197059102d0fbfb6
Rohan Magar <63698753+magarohan@users.noreply.github.com>
2025-12-11 16:31:54 +0545

    feat[category]: adds category grid to home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -25,6 +26,46 @@ class _HomePageState
   _currentIndex =
       0;
 
+  List<
+    String
+  >
+  hoodies = [
+    'https://i.pinimg.com/736x/ea/eb/67/eaeb67227e5d6db3e79f62f2b6162bf0.jpg',
+    'https://i.pinimg.com/736x/ea/eb/67/eaeb67227e5d6db3e79f62f2b6162bf0.jpg',
+    'https://i.pinimg.com/736x/04/3b/4f/043b4fa5b4de839beb5ff5d6f7119a3c.jpg',
+    'https://i.pinimg.com/736x/44/78/f9/4478f9b1d1added360fc2c95047af4be.jpg',
+  ];
+
+  List<
+    String
+  >
+  shoes = [
+    'https://images.pexels.com/photos/19090/pexels-photo.jpg',
+    'https://images.pexels.com/photos/1598505/pexels-photo-1598505.jpeg',
+    'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg',
+    'https://images.pexels.com/photos/19090/pexels-photo.jpg',
+  ];
+
+  List<
+    String
+  >
+  shirts = [
+    'https://i.pinimg.com/1200x/d4/08/b4/d408b4642bd57481ac5e3e972fb8d8e6.jpg',
+    'https://i.pinimg.com/1200x/97/f6/1b/97f61b80dc2b0ad8858a681a0d0c4597.jpg',
+    'https://i.pinimg.com/1200x/77/27/76/772776f5b6209709a2b989d411965487.jpg',
+    'https://i.pinimg.com/1200x/20/7e/87/207e874313d2822ecb5fbe4123297e47.jpg',
+  ];
+
+  List<
+    String
+  >
+  pants = [
+    'https://i.pinimg.com/1200x/a9/24/1c/a9241cd927deec3e8797426c03f541e3.jpg',
+    'https://i.pinimg.com/736x/25/9b/f2/259bf2e9bc4d1b85535ee8d947007a65.jpg',
+    'https://i.pinimg.com/1200x/db/a5/2a/dba52a6e2cd2f5832b748214291075ee.jpg',
+    'https://i.pinimg.com/1200x/ea/88/51/ea885190bf37c9b2152e8141447e46bc.jpg',
+  ];
+
   @override
   Widget
   build(
@@ -33,205 +74,137 @@ class _HomePageState
   ) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.w,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  backgroundImage: const NetworkImage(
-                    'https://i.pinimg.com/736x/25/33/8f/25338f488af2c45912c15ebab325e363.jpg',
-                  ),
-                  radius: 20.r,
+                SizedBox(
+                  height: 12.h,
                 ),
-                Container(
-                  width: 115.w,
-                  height: 35.h,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      18,
-                    ),
-                    color: const Color(
-                      0xFF004CFF,
-                    ),
-                  ),
-                  child: Text(
-                    'My Activity',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Raleway',
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                _circleIcon(
-                  'assets/icons/vouchers.png',
-                ),
-                _circleIcon(
-                  'assets/icons/menu.png',
-                ),
-                _circleIcon(
-                  'assets/icons/settings.png',
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 22.h,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Hello, User!',
-                style: TextStyle(
-                  color: const Color(
-                    0xFF202020,
-                  ),
-                  fontFamily: 'Raleway',
-                  fontSize: 28.sp,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 12.h,
-            ),
-            Container(
-              color: const Color(
-                0xFFF8F8F8,
-              ),
-              height: 70.h,
-              width: 335.w,
-              child: Column(
-                children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      'Announcements',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Shop',
                       style: TextStyle(
-                        fontFamily: 'Raileway',
-                        fontSize: 14.sp,
+                        fontFamily: 'Raleway',
+                        fontSize: 28.sp,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing \n elit. Maecenas hendrerit luctus libero ac vulputate.',
-                        style: TextStyle(
-                          fontFamily: 'NunitoSans',
-                          fontSize: 10.sp,
+                    Container(
+                      height: 36.h,
+                      width: 240.w,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12.w,
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color(
+                          0xFFF8F8F8,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          18.r,
                         ),
                       ),
-                      Container(
-                        height: 30.h,
-                        width: 30.w,
-                        decoration: const BoxDecoration(
-                          color: Color(
-                            0xFF0C54FF,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Search',
+                            style: TextStyle(
+                              fontFamily: 'Raleway',
+                              fontSize: 16.sp,
+                              color: const Color(
+                                0xFFC7C7C7,
+                              ),
+                            ),
                           ),
-                          shape: BoxShape.circle,
-                        ),
-                        child: Icon(
-                          Icons.arrow_forward_ios,
-                          size: 12.sp,
-                          color: const Color(
-                            0xFFFFFFFF,
+                          Icon(
+                            Icons.camera_alt_outlined,
+                            size: 20.sp,
+                            color: const Color(
+                              0xFF0042E0,
+                            ),
                           ),
-                        ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                _sectionTitle(
+                  'Categories',
+                ),
+                SizedBox(
+                  height: 16.h,
+                ),
+                SizedBox(
+                  width: 335.w,
+                  height: 636.h,
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 20.h,
+                    crossAxisSpacing: 20.w,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      _gridCategory(
+                        'Hoodies',
+                        hoodies,
+                      ),
+                      _gridCategory(
+                        'Shoes',
+                        shoes,
+                      ),
+                      _gridCategory(
+                        'Shirts',
+                        shirts,
+                      ),
+                      _gridCategory(
+                        'Pants',
+                        pants,
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 18.h,
-            ),
-            _sectionTitle(
-              'Recently viewed',
-            ),
-            SizedBox(
-              height: 12.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                5,
-                (
-                  index,
-                ) => CircleAvatar(
-                  backgroundImage: const NetworkImage(
-                    'https://i.pinimg.com/736x/25/33/8f/25338f488af2c45912c15ebab325e363.jpg',
-                  ),
-                  radius: 25.r,
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 25.h,
-            ),
-            _sectionTitle(
-              'My Orders',
-            ),
-            SizedBox(
-              height: 12.h,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                _OrderChip(
-                  text: 'To Pay',
+
+                SizedBox(
+                  height: 24.h,
                 ),
-                _OrderChip(
-                  text: 'To Receive',
+                _sectionTitle(
+                  'Top Products',
                 ),
-                _OrderChip(
-                  text: 'To Review',
+                SizedBox(
+                  height: 12.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _circleIcon(
+                      "https://i.pinimg.com/736x/0e/6a/24/0e6a24a0524c589f60c758c851ebdcd7.jpg",
+                    ),
+                    _circleIcon(
+                      "https://i.pinimg.com/1200x/08/e7/0a/08e70a94d43b007efda8ef04962e4895.jpg",
+                    ),
+                    _circleIcon(
+                      "https://i.pinimg.com/1200x/a5/22/90/a52290bfc7b6ed20e7da47719ae362a3.jpg",
+                    ),
+                    _circleIcon(
+                      "https://i.pinimg.com/1200x/a1/7d/9a/a17d9a900eea4d103ca31d2a3d25483c.jpg",
+                    ),
+                    _circleIcon(
+                      "https://i.pinimg.com/1200x/d4/08/b4/d408b4642bd57481ac5e3e972fb8d8e6.jpg",
+                    ),
+                  ],
                 ),
               ],
             ),
-            SizedBox(
-              height: 28.h,
-            ),
-            _sectionTitle(
-              'Stories',
-            ),
-            SizedBox(
-              height: 5.h,
-            ),
-            SizedBox(
-              height: 175.h,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder:
-                    (
-                      context,
-                      index,
-                    ) {
-                      return Container(
-                        height: 175.h,
-                        width: 104.w,
-                        margin: EdgeInsets.only(
-                          right: 8.w,
-                        ),
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              'https://i.pinimg.com/736x/25/33/8f/25338f488af2c45912c15ebab325e363.jpg',
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      );
-                    },
-              ),
-            ),
-          ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -283,17 +256,25 @@ class _HomePageState
   Widget
   _circleIcon(
     String
-    asset,
+    link,
   ) {
     return Container(
+      height: 55.w,
+      width: 55.w,
       decoration: const BoxDecoration(
         shape: BoxShape.circle,
         color: Color(
           0xFFF8F8F8,
         ),
       ),
-      child: Image.asset(
-        asset,
+      padding: EdgeInsets.all(
+        8.w,
+      ),
+      child: ClipOval(
+        child: Image.network(
+          link,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
@@ -303,60 +284,64 @@ class _HomePageState
     String
     text,
   ) {
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Text(
-        text,
-        style: TextStyle(
-          fontFamily: 'Raleway',
-          fontSize: 21.sp,
-          fontWeight: FontWeight.bold,
-        ),
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Raleway',
+        fontSize: 21.sp,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
-}
 
-class _OrderChip
-    extends
-        StatelessWidget {
-  final String
-  text;
-
-  const _OrderChip({
-    required this.text,
-  });
-
-  @override
   Widget
-  build(
-    BuildContext
-    context,
+  _gridCategory(
+    String
+    title,
+    List<
+      String
+    >
+    images,
   ) {
-    return Container(
-      margin: EdgeInsets.only(
-        right: 8.w,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(
-          0xFFE5EBFC,
-        ),
-        borderRadius: BorderRadius.circular(
-          35.r,
-        ),
-      ),
-      height: 35.h,
-      width: 86.w,
-      alignment: Alignment.center,
-      child: Text(
-        text,
-        style: TextStyle(
-          fontFamily: 'Raleway',
-          fontSize: 16.sp,
-          color: const Color(
-            0xFF0042E0,
+    return SizedBox(
+      width: 165.w,
+      height: 192.h,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 150.h,
+            child: GridView.count(
+              crossAxisCount: 2,
+              physics: NeverScrollableScrollPhysics(),
+              mainAxisSpacing: 6,
+              crossAxisSpacing: 6,
+              children: List.generate(
+                4,
+                (
+                  index,
+                ) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      5.r,
+                    ),
+                  ),
+                  child: Image.network(
+                    images[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
           ),
-        ),
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: 'Raleway',
+              fontSize: 17.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
